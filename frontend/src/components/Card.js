0cede2113d3bb5d0b42b77141f9652afbb84af3d
Ankit Madhukar <adityamadhukar01@gmail.com>
2023-12-11 22:09:27 +0530

    .

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,14 +1,32 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import EditModal from './EditModal';
 const Card = (props) => {
-    const {first_name, last_name, email, gender, avatar, domain, available} = props.item;
+    const { _id, first_name, last_name, email, gender, avatar, domain, available } = props.item;
     const [edModal, setEdModal] = useState(false);
     const handleEdit = () => {
         setEdModal(true);
     }
+    const handleDelete = async () => {
+        const res = await fetch(`https://userdb-backend.onrender.com/delete/${_id}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        const data = await res.json(JSON.stringify(res));
+        console.log(data);
+        if (res.status == 404 || !data) {
+            alert(data)
+        }
+        else {
+            alert("User Deleted");
+            // props.setEdModal(false);
+            window.location.reload();
+        }
+    }
     return (
         <div>
-            {edModal&&<EditModal setEdModal={setEdModal} item={props.item}/>}
+            {edModal && <EditModal setEdModal={setEdModal} item={props.item} />}
             <div class="w-20%  bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                 {/* <div class="flex justify-end px-4 pt-4">
                     <button id="dropdownButton" data-dropdown-toggle="dropdown" class="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5" type="button">
@@ -37,8 +55,12 @@ const Card = (props) => {
                     <span class="text-sm text-gray-500 dark:text-gray-400">{domain}</span>
                     <div class="flex mt-4 md:mt-6">
                         <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to Team</a>
-                        <a href="#" onClick={()=>{handleEdit()}} class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700 ms-3">Edit</a>
                     </div>
+                    <div class="flex mt-4 md:mt-6">
+                        <a href="#" onClick={() => { handleEdit() }} class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700 ms-3">Edit</a>
+                        <a href="#" onClick={() => { handleDelete() }} class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700 ms-3">Delete</a>
+                    </div>
+
                 </div>
             </div>
 
